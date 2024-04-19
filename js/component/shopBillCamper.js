bac698efac22e0d9823409004bcdc5b832bf125e
Sebastian08LR <juansebastianlatorreruiz@gmail.com>
2024-04-19 12:25:58 -0500

    feat: :construction: add end points and rendering information

--- js/component/shopBillCamper.js
+++ js/component/shopBillCamper.js
@@ -1,3 +1,5 @@
-export const example = ()=>{
-    return "Hello world"
+export const getAllProductBill = async()=>{
+    let conetion = await fetch("http://localhost:3000/camper")
+    let data = await conetion.json();
+    return data;
 }
