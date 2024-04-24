e038f6895f262018b70b38540731e52f4e0b5383
Sebastian08LR <156454864+Sebastian08LR@users.noreply.github.com>
2024-04-24 10:24:05 -0500

    feat: :sparkles: Soluciona las consultas sobre los pagos

---
+++ js/module/payments.js
@@ -0,0 +1,28 @@
+export const getAllOrdersPaidIn2008WithPayPal = async()=>{
+    let res = await fetch("http://localhost:5505/payments")
+    let data = await res.json();
+    let dataUpdate = [];
+    data.forEach(payment =>{
+        let payment_date = null;
+        if(payment.date_payment){
+            payment_date = (payment.date_payment.split('-'))[0] 
+        }
+        if( payment_date == "2008" && payment.payment == "PayPal"){
+                dataUpdate.push(payment);
+        }
+    })
+   /*  dataUpdate = [...new Set(dataUpdate)] */
+   return dataUpdate.reverse();
+}
+
+export const getAllPossiblePaymentsMethods = async()=>{
+    let res = await fetch("http://localhost:5505/payments")
+    let data = await res.json();
+    let dataUpdate = [];
+    data.forEach(payment =>{
+                dataUpdate.push(payment.payment);
+    })
+    dataUpdate = [...new Set(dataUpdate)]
+    
+    return dataUpdate;
+}
